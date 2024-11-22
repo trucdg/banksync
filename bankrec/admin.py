26e76741d1f6a5ad3b87a22f01377bd07a60715e
Truc Duong <trucdg@bu.edu>
2024-11-22 18:40:44 -0500

    add some initial Users data and update stylings

--- bankrec/admin.py
+++ bankrec/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(BusinessActivity)
+admin.site.register(Category)
+admin.site.register(BusinessActivityLink)
+admin.site.register(BusinessCategory)
+admin.site.register(TransactionEntry)
